2cd722ccfcf516c7b2eb7c1377bacf7a3fb95b29
justineAm <78581939+justineAm@users.noreply.github.com>
2021-05-06 14:02:53 +0800

    the crud object has been added

---
+++ CrudUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CrudJframe;
+
+/**
+ *
+ * @author 2ndyrGroupA
+ */
+public class CrudUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Main().setVisible(true);
+    }  
+}
